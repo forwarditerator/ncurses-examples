5a0eaa70aa7f967c3a81fae2416d6d49bd3972fa
Carlos Lemos <forwarditerator@gmail.com>
2018-09-22 13:35:54 -0400

    beep() flash()

---
+++ notice.c
@@ -0,0 +1,15 @@
+#include <ncurses.h>
+
+int main() {
+	initscr();
+	addstr("Attention!\n");
+	beep();
+	refresh();
+	getch();
+	addstr("I said, ATTENTION!\n");
+	flash();
+	refresh();
+	getch();
+	endwin();
+	return 0;
+}
