3e658ca17e36abf9ec6bc6c88873ade74b1e2fa0
Carlos Lemos <forwarditerator@gmail.com>
2018-09-22 17:28:46 -0400

    center(line, string)

---
+++ ctitle.c
@@ -0,0 +1,22 @@
+#include <ncurses.h>
+#include <string.h>
+
+void center(int row, char* title) {
+	int len, indent, y, width;
+	getmaxyx(stdscr, y, width);
+	len = strlen(title);
+	indent = (width - len)/2;
+	mvaddstr(row, indent, title);
+	refresh();
+}
+
+int main(){
+	initscr();
+	center(1, "Penguin Soccer Finals");
+	center(5, "Cattle Dung Samples from Temecula");
+	center(7, "Catatonic Theater");
+	center(9, "Why Do Icons Hate Each Other?");
+	getch();
+	endwin();
+	return 0;
+}
