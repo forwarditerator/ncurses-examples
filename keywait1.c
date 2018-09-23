8b5c8eb97c40a9fcdd13ce76174ab5e9c3c86840
Carlos Lemos <forwarditerator@gmail.com>
2018-09-23 16:14:13 -0400

    nodelay()

---
+++ keywait1.c
@@ -0,0 +1,16 @@
+#include <ncurses.h>
+int main() {
+	int value = 0;
+	initscr();
+	addstr("Press any key to begin:\n");
+	refresh();
+	getch();
+	nodelay(stdscr, TRUE);
+	addstr("Press any key to stop the insane loop!\n");
+	while(getch() == ERR) {
+		printw("%d\r", value++);
+		refresh();
+	}
+	endwin();
+	return 0;
+}
