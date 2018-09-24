06ea4417c12fbae39333ac45a777e349479131f4
Carlos Lemos <forwarditerator@gmail.com>
2018-09-24 15:19:46 -0400

    delwin()

---
+++ twowin2.c
@@ -0,0 +1,23 @@
+#include <ncurses.h>
+int main() {
+	WINDOW* two;
+	initscr();
+	addstr("This is the original window, stdscr.\n");
+	refresh();
+	getch();
+	two = newwin(0,0,0,0);
+	if (two == NULL) {
+		addstr("Unable to allocate memory for new window.");
+		endwin();
+		return(1);
+	}
+	waddstr(two,"This is the new window created!\n");
+	wrefresh(two);
+	getch();
+	delwin(two);
+	addstr("The second window was removed.\n");
+	refresh();
+	getch();
+	endwin();
+	return 0;
+}
